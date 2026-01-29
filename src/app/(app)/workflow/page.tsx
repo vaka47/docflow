f99514e5956ed5c582f74c5020082b4c795a795a
Ivan Safonov <safonov47@gmail.com>
2026-01-29 18:18:20 +0300

    Wrap workflow board in Suspense

--- src/app/(app)/workflow/page.tsx
+++ src/app/(app)/workflow/page.tsx
@@ -2,11 +2,14 @@ import WorkflowBoardClient from "./WorkflowBoardClient";
 import WorkflowArtifactsClient from "./WorkflowArtifactsClient";
 import WorkflowCalendarClient from "./WorkflowCalendarClient";
 import ApprovalInboxClient from "./ApprovalInboxClient";
+import { Suspense } from "react";
 
 export default function WorkflowPage() {
   return (
     <div className="grid gap-8">
-      <WorkflowBoardClient />
+      <Suspense fallback={<div className="surface p-6 text-sm text-black/50">Загрузка...</div>}>
+        <WorkflowBoardClient />
+      </Suspense>
 
       <WorkflowCalendarClient />
 
